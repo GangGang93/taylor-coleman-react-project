aee4f95d6483594e4b62888f6b3ee850a595aec7
GangGang93 <taylorlouisecoleman@gmail.com>
2020-04-06 17:14:15 -0400

    another commit message here

---
+++ src/components/navigation/navigation-container.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+
+export default class NavigationComponent extends Component {
+  constructor(props) {
+      super(props)
+
+      this.state = {
+          
+      }
+  }
+
+  render() {
+      return (
+        <div className="nav-wrapper">
+            <div className="left-side">
+                <div className="nav-link-wrapper">
+                <NavLink exact to="/" activeClassName="nav-link-active">Home</NavLink>
+                </div>
+                <div className="nav-link-wrapper">
+                <NavLink to="/about" activeClassName="nav-link-active">About</NavLink>
+                </div>
+                <div className="nav-link-wrapper">
+                <NavLink to="/contact" activeClassName="nav-link-active">Contact</NavLink>
+                </div>
+                <div className="nav-link-wrapper">
+                <NavLink to="/blog" activeClassName="nav-link-active">Blog</NavLink>
+                </div>
+
+                {false ? <button>Add Blog</button> : null } 
+            </div>
+
+            <div className="right-side">
+                TAYLOR COLEMAN
+            </div>
+        </div>
+      )
+  }
+}
