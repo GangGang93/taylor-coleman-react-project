aac79fc70f0e0649a5875a9f39a9376ccf859149
GangGang93 <taylorlouisecoleman@gmail.com>
2020-04-07 17:34:20 -0400

    5ello from the 6

--- src/components/app.js
+++ src/components/app.js
@@ -12,10 +12,31 @@ import About from "./pages/about";
 import Contact from "./pages/contact";
 import Blog from "./pages/blog";
 import PortfolioDetail from "./portfolio/portfolio-detail";
+import Auth from "./pages/auth";
 import NoMatch from "./pages/no-match";
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      loggedInStatus: "NOT_LOGGED_IN"
+    };
+
+    this.handleSuccessfulLogin=this.handleSuccessfulLogin.bind(this);
+    this.handleUnSuccessfulLogin=this.handleUnSuccessfulLogin.bind(this);  }
+  
+  handleSuccessfulLogin() {
+    this.setState({
+      loggedInStatus: "LOGGED_IN"
+    });
+  }
+
+  handleUnSuccessfulLogin() {
+      this.setState({
+        loggedInStatus: "NOT_LOGGED_IN"
+      })
+  }
   render() {
     return (
       <div className='container'>
@@ -27,6 +48,18 @@ export default class App extends Component {
 
               <Switch>
                 <Route exact path="/" component={Home} />
+
+                <Route
+                 path="/auth" 
+                 redner={props => (
+                   <Auth 
+                     {...props}
+                     handleSuccessfulLogin={this.handleSuccessfulLogin}
+                     handleUnSuccessfulLogin={this.handleUnSuccessfulLogin}
+                     />
+                 )}
+                 />
+
                 <Route path="/about-me" component={About} />
                 <Route path="/contact" component={Contact} />
                 <Route path="/blog" component={Blog} />
